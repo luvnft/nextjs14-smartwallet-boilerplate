b60c72e150f690c80b424843a8c582d525a84e09
rokitgg <pintosdsgn@gmail.com>
2024-05-03 00:42:06 +0200

    update: navbar component

--- components/feature/layout/navbar/navbar.tsx
+++ components/feature/layout/navbar/navbar.tsx
@@ -19,7 +19,7 @@ const Navbar: React.FC<NavbarProps> = ({ appName }) => {
     <nav className="fixed w-full flex items-center justify-between flex-wrap border-b p-6 bg-background">
       <div className="flex justify-between w-full items-center">
         <div className="flex items-center flex-shrink-0 mr-6">
-          <span className="font-semibold text-xl tracking-tight">
+          <span className="font-semibold md:text-xl tracking-tight">
             {appName}
           </span>
         </div>
