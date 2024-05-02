b015b3f4a389871488e6a20f8edf1310429625f1
rokitgg <pintosdsgn@gmail.com>
2024-05-03 00:41:54 +0200

    add: site config file

---
+++ config/site.ts
@@ -0,0 +1,16 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: 'Next 14 - Smart Wallet Boilerplate',
+  description: '',
+  mainNav: [
+    {
+      title: 'Home',
+      href: '/',
+    },
+  ],
+  links: {
+    twitter: 'https://twitter.com/fewmoreliqs',
+    github: 'https://github.com/rokitgg/nextjs14-smartwallet-boilerplate',
+  },
+};
